813ed73b9a1b56f963cc86ec05d9f09378460362
kartikgadhiya24-dev <kartikgadhiya24@gmail.com>
2026-09-22 16:48:46 +0530

    Add ScrollToTop component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,9 +11,20 @@ import Contact from './pages/Contact';
 import Appointment from './pages/Appointment';
 import NotFound from './pages/NotFound';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="app-layout">
         <Header />
         <main className="main-content">
